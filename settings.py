f4639736ceb873a0a0e6d3adbdb22f3af7e0705b
evnsmith <68720760+evnsmith@users.noreply.github.com>
2022-01-19 14:51:59 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,25 +4,25 @@ import platform
 currentPath = os.path.dirname(os.path.realpath(__file__))
 
 #Spotify API
-SPOTIPY_USER_ID="evnsmith"
-SPOTIPY_CLIENT_ID="7dd262ded5444e23a522a7ec80a337b3"
-SPOTIPY_CLIENT_SECRET="0d41a755472f4ba28c32edbe2ed3807f"
+SPOTIPY_USER_ID=""
+SPOTIPY_CLIENT_ID=""
+SPOTIPY_CLIENT_SECRET=""
 REDIRECT_URI="http://localhost"
 SCOPE = "user-library-read"
 
 #Genius API
-genius_client_id = 'CW5jD19D4E5c333ABbc8yE9J24G13Kv203P2MDdd_BtZKin4RaktnIBkTph_C65T'
-genius_client_secret = 'c1cj2cqxOZh6wqyOqOZ3mCsIstQKMrTxmYJTRQAHa3Og6D4fLsEGk5MCH6TZMiVhLEuip2le5s5AmqiLfjpXwg'
-genius_access_token = 'DO63H-OZYinSUB-_oMXZyJpX-AOJNf3IWhGZIXAc55jEsDTU19tsLGBIsGC4mnCb'
+genius_client_id = ''
+genius_client_secret = ''
+genius_access_token = ''
 
 #mysql database
 databasehost = "localhost"
 databaseuser = "root"
-databasepassword = "Es90290!Es90290!"
+databasepassword = ""
 
 input_image_path = '\\Video\\album covers\\'
-output_image_path = 'C:\\Users\\evans\\PycharmProjects\\Spotify-to-yt\\Video\\album covers\\'
-audio_path = 'C:\\Users\\evans\\PycharmProjects\\Spotify-to-yt\\Video\\audio'
-final_video_path = 'C:\\Users\\evans\\PycharmProjects\\Spotify-to-yt\\Video\\final_videos\\'
+output_image_path = '\\Video\\album covers\\'
+audio_path = '\\Video\\audio'
+final_video_path = '\\Video\\final_videos\\'
 
-google_cred_upload = 'C:\\Users\evans\\PycharmProjects\\Spotify-to-yt\\SpotifyDl\\client_secrets.json'
+google_cred_upload = 'client_secrets.json'
